f3ddc9b909adc98654b8e20c849fcbf9b7ab007e
snwessel <wessel.s@husky.neu.edu>
2020-12-22 16:57:34 -0500

    Implement hacker actions and define primary cta

--- src/components/HackerActions.js
+++ src/components/HackerActions.js
@@ -2,13 +2,29 @@ import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 
 const HackerActions = () => (
-  <Container className="mt-5">
+  <Container className="my-5">
     <Row>
       <Col>
         <h3>Send us your feedback</h3>
+        <p>
+            This is a blub about the core team and why we love hackbeanpot 
+            and organizing the event! This is a blub about the core team 
+            and why we love hackbeanpot and organizing the event!
+        </p>
+        <a href="#">
+            <button type="button" class="btn primary-cta">Send Feedback</button>
+        </a>
       </Col>
       <Col>
         <h3>Apply to join Core!</h3>
+        <p>
+            This is a blub about the core team and why we love hackbeanpot 
+            and organizing the event! This is a blub about the core team 
+            and why we love hackbeanpot and organizing the event!
+        </p>
+        <a href="#">
+            <button type="button" class="btn primary-cta">Apply</button>
+        </a>
       </Col>
     </Row>
   </Container>
