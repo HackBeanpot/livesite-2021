a4291800a959f8a34d2a34a8ace614451c3e6024
daniaalnahdi <33553341+daniaalnahdi@users.noreply.github.com>
2020-12-29 23:46:25 +0300

    add custom airtable api hook (#4)
    * AT SW fix links for build deployment
    
    * add custom airtable api hook
    
    Co-authored-by: meebs <alex@takayamasan.com>
    Co-authored-by: Sarah Wessel <snwessel@gmail.com>

---
+++ src/hooks/api-hook.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+const useAirtableAPI = (basePath, tableQuery) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+      const url = new URL(`https://api.airtable.com/v0/${basePath}/${tableQuery}?api_key=${process.env.REACT_APP_AIRTABLE_KEY}`);
+
+      const getData = async () => {
+        const responseData = await fetch(url);
+        const responseJson = await responseData.json();
+        setIsLoading(false)
+        setData(responseJson.records);
+      }
+
+      try {
+        getData();
+      } catch (err) {
+        setIsLoading(false)
+        console.log(err);
+      }
+  }, []);
+
+  return { data, isLoading };
+};
+
+export default useAirtableAPI;
