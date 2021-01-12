566f4a9facaa074b81117b0353f647347f3ccbc9
snwessel <wessel.s@husky.neu.edu>
2021-01-12 13:54:41 -0500

    Get countdown working, hide on small screens

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import useCounter from '../hooks/counter-hook';
 
 export default function Countdown() {
-  return <div className="countdown">
-    <div className="countdown__text-wrapper">
-      <div className="countdown__text">
-        Hacking time remaining
+  const timeleft = useCounter();
+
+  return (
+    <div className="countdown">
+      <div className="countdown__text-wrapper">
+        <div className="countdown__text">
+          Hacking time remaining
+        </div>
+        <div id="clock" className="font-weight-bold countdown__timer">
+          {timeleft.days}:{timeleft.hours}:{timeleft.minutes}:{timeleft.seconds}
+        </div>
       </div>
-      <div className="font-weight-bold countdown__timer">
-        03:34:18
-      </div>
+      <script src="../scripts/countdown.js"></script>
     </div>
-  </div>;
-}
+  );
+};
