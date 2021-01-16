a9460233b6dff795a3c2ed371a3a2d3fb14db978
Cindy Luo <luo.xiny@northeastern.edu>
2021-01-16 12:57:22 -0500

    welcome intro text + link

--- src/components/Relevant.js
+++ src/components/Relevant.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
-const Header = () => (
+const Relevant = () => (
   <Container className="mt-4">
     <Row>
       <Col className="text-center">
-        <h1 className="font-weight-bold">Welcome to HackBeanpot 2021!</h1>
+        <h1 className="font-weight-bold">Relevant right now!</h1>
       </Col>
     </Row>
   </Container>
 );
 
-export default Header;
+export default Relevant;
