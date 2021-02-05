48c41cf47e2f5cb5fa81d4da8d31c0d4f2c944c9
Shania Ambros <67839745+Ulam22@users.noreply.github.com>
2021-02-05 13:38:47 -0500

    added footer (#13)
    * footer styled like the main site
    
    * viewable icons and legal info
    
    * added link to newsletter
    
    * added newsletter link
    
    * removed css folder
    
    * there's a problem
    
    * Delete the couple of commented out lines
    
    Co-authored-by: Nupur Neogi <33645465+nupur-n@users.noreply.github.com>
    Co-authored-by: snwessel <wessel.s@husky.neu.edu>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,56 @@
 import React from "react";
 
-const Footer = () => <div className=""></div>;
+const Footer = () => (
+  <div className="footer">
+    <div className="newsletter">
+      <a href="https://hackbeanpot.us10.list-manage.com/subscribe/post?u=a98050d47fdae2481521f0474&amp;id=dccd8c8431">
+        Sign up for our Newsletter
+      </a>
+    </div>
+    <div className="footer__socials">
+      <a
+        href="https://www.facebook.com/hackbeanpot/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fab fa-facebook  fa-2x footer-social-icon footer-text"></i>
+      </a>
+      <a
+        href="https://www.instagram.com/hackbeanpot/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fab fa-instagram fa-2x footer-social-icon footer-text"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/company/9315150/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fab fa-linkedin fa-2x footer-social-icon footer-text"></i>
+      </a>
+      <a
+        href="https://hackbeanpot.medium.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fab fa-medium-m  fa-2x footer-social-icon footer-text"></i>
+      </a>
+
+      <a
+        href="https://twitter.com/hackbeanpot"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fab fa-twitter  fa-2x footer-social-icon footer-text"></i>
+      </a>
+    </div>
+
+    <div className="footer__legal">
+      <p>HackBeanpot, Inc. is a registered 501(c)(3) organization. </p>
+      <a href="https://hackbeanpot.com/code-of-conduct">Code of Conduct</a>
+    </div>
+  </div>
+);
 
 export default Footer;
