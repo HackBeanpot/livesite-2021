5f46249af348d27cf82be2b2269cbb4f6f38edc8
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2021-02-20 17:44:27 -0500

    update project parser rows.

--- src/data/judging/projectParser.py
+++ src/data/judging/projectParser.py
@@ -10,13 +10,13 @@ with open('projects.csv') as csv_file:
     for row in csv_reader:
         if line_count == 0:
             line_count += 1
-        elif row[10] != SUBMISSION_PASSWORD: # TODO: Change this once we know what row it's supposed to be
+        elif row[16] != SUBMISSION_PASSWORD:  # TODO: Change this once we know what row it's supposed to be
             rows.append({
                 "projectName": row[0],
                 "submissionLink": row[1],
                 "tryItOutLink": row[7],
                 "videoDemoLink": row[8],
-                "liveDemo": row[9]
+                "liveDemo": row[15]
             })
 
 with open('projectData.json', 'w') as outfile:
