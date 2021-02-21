d1d46b3d52cf65329c6e316cc2705201d2bb92c5
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2021-02-21 09:54:13 -0500

    hope this works.

--- src/data/judging/projectParser.py
+++ src/data/judging/projectParser.py
@@ -10,13 +10,13 @@ with open('projects.csv') as csv_file:
     for row in csv_reader:
         if line_count == 0:
             line_count += 1
-        elif row[16] != SUBMISSION_PASSWORD:  # TODO: Change this once we know what row it's supposed to be
+        elif (row[15] == SUBMISSION_PASSWORD or row[15] == 'Beans@2021') and row[2] != 'Draft':
             rows.append({
                 "projectName": row[0],
                 "submissionLink": row[1],
                 "tryItOutLink": row[7],
                 "videoDemoLink": row[8],
-                "liveDemo": row[15]
+                "liveDemo": row[16]
             })
 
 with open('projectData.json', 'w') as outfile:
