f447a14ef3e310d204395d870b85273b2cf89834
felicia <zhang.fel@northeastern.edu>
2021-02-23 23:44:57 -0500

    mobile

--- src/components/Thanks.js
+++ src/components/Thanks.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import Confetti from 'react-confetti'
+import React from "react";
+import Confetti from "react-confetti";
 import { useParams } from "react-router";
 import TheSummit from "../assets/TheSummit.png";
 
 const Thanks = () => {
   const { name } = useParams();
-  
+
   return (
     <>
       <Confetti />
       <div className="thanks">
         <div className="thanks__body">
-          <img className="thanks__img" src={TheSummit} alt="HBP summit"/>
+          <img className="thanks__img" src={TheSummit} alt="HBP summit" />
           <h1>{`Thank you, ${name}!`}</h1>
-          <h4>It was great having you at HackBeanpot 2021. Hope to see you again next year!</h4>
+          <h4>
+            It was great having you at HackBeanpot 2021. Hope to see you again
+            next year!
+          </h4>
         </div>
       </div>
     </>
-  )
-
-}
-
+  );
+};
 
 export default Thanks;
